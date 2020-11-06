df5f29f261a457616f1493baeda8cd0537248fa7
Even <765305261@qq.com>
2020-11-06 16:06:16 +0800

    Add code for MOT evaluation(MOT16).

--- MOTEvaluate/evaluate_pipeline.py
+++ MOTEvaluate/evaluate_pipeline.py
@@ -2,5 +2,20 @@
 
 import os
 
+
+def evaluate_test_set(test_root):
+    """
+    :param test_root:
+    :return:
+    """
+    # Check test root for video and darklabel format label file(txt)
+
+    # Convert darklabel label file to mot16 format
+
+    # Call mcmot_yolov4(demo.py) to do tracking(generate results.txt)
+
+    # Do evaluation using results and ground truth for each seq
+
 if __name__ == '__main__':
+
     print('Done.')
