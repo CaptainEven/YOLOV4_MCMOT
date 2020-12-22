31e3de882b55431e2d0a13dddd8194197c7a4aed
Even <765305261@qq.com>
2020-12-22 16:16:47 +0800

    Add code for feature vector embedding testing(greedy matching)_add code for correct/wrong matching visualization.

--- TestFeatureMatch.py
+++ TestFeatureMatch.py
@@ -171,6 +171,14 @@ class FeatureMatcher(object):
 
             self.dataset = LoadImages(video_path, self.opt.img_proc_method, self.opt.net_w, self.opt.net_h)
 
+            # create viz dir
+            if viz_dir != None:
+                if not os.path.isdir(viz_dir):
+                    os.makedirs(viz_dir)
+                else:
+                    shutil.rmtree(viz_dir)
+                    os.makedirs(viz_dir)
+
             print('Run seq {:s}...'.format(video_path))
             precision = self.run_a_seq(cls_id, img_w, img_h, viz_dir)
             mean_precision += precision
@@ -398,14 +406,6 @@ class FeatureMatcher(object):
         :param viz_dir:
         :return:
         """
-        # create viz dir
-        if viz_dir != None:
-            if not os.path.isdir(viz_dir):
-                os.makedirs(viz_dir)
-            else:
-                shutil.rmtree(viz_dir)
-                os.makedirs(viz_dir)
-
         # define image width and height
         self.img_w, self.img_h = img_w, img_h
 
