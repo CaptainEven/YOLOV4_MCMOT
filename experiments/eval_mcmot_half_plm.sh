f72862246594dc5ad0aa5d233a2e0b212792c57c
Even <765305261@qq.com>
2021-05-28 14:38:24 +0800

    Refactor.

---
+++ experiments/eval_mcmot_half_plm.sh
@@ -0,0 +1,3 @@
+python3 ./MOTEvaluate/evaluate_feature_matching.py --cfg ./cfg/yolov4_half_plm_one_feat_fuse.cfg \
+                                                   --weights ./weights/mcmot_half_plm_track_last.weights \
+                                                   --names ./data/mcmot.names
